8414bc19faee2ee2debdff54e7042ef6a87a5235
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2025-08-13 20:04:17 +0200

    Clean up

--- code/compiler/ext/generateparser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -jar antlr4/antlr4.jar -Dlanguage=Cpp -Xexact-output-dir -o ./../parser4/ antlr4/grammar/GPULang.g4 
--- code/compiler/parser4/GPULangBaseListener.cpp
+++
@@ -1,7 +0,0 @@
-
-// Generated from antlr4/grammar/GPULang.g4 by ANTLR 4.13.2
-
-
-#include "GPULangBaseListener.h"
-
-
