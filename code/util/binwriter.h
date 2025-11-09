1c1ac94866e6498f0c0a1ccdeff377ff29e444c2
Gustav Sterbrant <gustav.sterbrant@gmail.com>
2025-11-09 02:15:50 +0100

    Fix build on ubuntu

--- code/util/binwriter.h
+++ code/util/binwriter.h
@@ -10,6 +10,7 @@
 //------------------------------------------------------------------------------
 #include <iostream>
 #include <fstream>
+#include <cstdint>
 namespace GPULang
 {
 class BinWriter
